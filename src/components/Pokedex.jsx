637c502382a6773c9f4ab6dddf242652cc2c09f4
Nikola <mtsebrov@gmail.com>
2023-03-02 23:37:16 +0200

    fix style

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -77,8 +77,8 @@ class Pokedex extends React.Component {
                                             <Card onClick={() => this.showInfo(pokemon.id)} border="danger" key={pokemon.id} style={{ width: '12rem' }}>
                                                 <Card.Img variant="top" src={pokemon.image} />
                                                 <Card.Body>
-                                                    <Card.Title>{pokemon.name}</Card.Title>
-                                                    <Card.Text>
+                                                    <Card.Title className="Title">{pokemon.name}</Card.Title>
+                                                    <Card.Text className="Title">
                                                         {pokemon.type}
                                                     </Card.Text>
                                                 </Card.Body>
@@ -88,53 +88,52 @@ class Pokedex extends React.Component {
                                 </Row>
                             </Container>
                         </Col>
-                        <Col xs={6}>{this.state.show ?
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={this.state.show_pokemon?.image} />
-                                <Card.Body>
-                                    <Card.Title>{this.state.show_pokemon?.name} #{this.state.show_pokemon?.id}</Card.Title>
-                                    <Table bordered>
-                                        <tbody>
-                                            <tr>
-                                                <td>Type</td>
-                                                <td>{this.state.show_pokemon?.type}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Attack</td>
-                                                <td>{this.state.show_pokemon?.attack}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Defense</td>
-                                                <td>{this.state.show_pokemon?.defense}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>HP</td>
-                                                <td>{this.state.show_pokemon?.hp}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>SP Attack</td>
-                                                <td>{this.state.show_pokemon?.["special-attack"]}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>SP Defense</td>
-                                                <td>{this.state.show_pokemon?.['special-defense']}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Speed</td>
-                                                <td>{this.state.show_pokemon?.speed}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Weight</td>
-                                                <td>{this.state.show_pokemon?.weight}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Total moves</td>
-                                                <td>{this.state.show_pokemon?.moves}</td>
-                                            </tr>
-                                        </tbody>
-                                    </Table>
-                                </Card.Body>
-                            </Card>
+                        <Col xs={6}>{this.state.show ? <Card style={{ width: '18rem', marginTop: 50, marginBottom: 50 }}>
+                            < Card.Img variant="top" src={this.state.show_pokemon?.image} />
+                            <Card.Body>
+                                <Card.Title className="Title">{this.state.show_pokemon?.name} #{this.state.show_pokemon?.id}</Card.Title>
+                                <Table bordered>
+                                    <tbody>
+                                        <tr>
+                                            <td className='fixType'>Type</td>
+                                            <td className="Title">{this.state.show_pokemon?.type}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Attack</td>
+                                            <td>{this.state.show_pokemon?.attack}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Defense</td>
+                                            <td>{this.state.show_pokemon?.defense}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>HP</td>
+                                            <td>{this.state.show_pokemon?.hp}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>SP Attack</td>
+                                            <td>{this.state.show_pokemon?.["special-attack"]}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>SP Defense</td>
+                                            <td>{this.state.show_pokemon?.['special-defense']}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Speed</td>
+                                            <td>{this.state.show_pokemon?.speed}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Weight</td>
+                                            <td>{this.state.show_pokemon?.weight}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total moves</td>
+                                            <td>{this.state.show_pokemon?.moves}</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
                             : null}</Col>
                     </Row >
                 </Container >
